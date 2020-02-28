3d1973c93ece415d274f5a7437bad09e62fa7c92
btobers <btober@arizona.edu>
2020-02-28 08:03:03 -0700

    cleanup

--- SHARADreflectivity.sh
+++ SHARADreflectivity.sh
@@ -49,35 +49,35 @@ cd /zippy/MARS/code/supl/SHARAD/sharad-tools/rangeCompress/code/python
 echo "Range compression completed"
 echo "----------------------------------------------------------------"
 
-# cd /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/data/rgram/amp
+cd /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/data/rgram/amp
 
-# # get list of range compressed files for input to surface power script
-# find . -name "*.npy" -exec basename \{} \; > /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/rc_list_$DATE.txt
+# get list of range compressed files for input to surface power script
+find . -name "*.npy" -exec basename \{} \; > /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/rc_list_$DATE.txt
 
-# cd /zippy/MARS/code/supl/SHARAD/sharad-tools
+cd /zippy/MARS/code/supl/SHARAD/sharad-tools
 
-# echo "Calculating surface reflectivity"
-# echo "Surface type: $SURFTYPE"
-# echo "Window: $WINDOW"
-# echo "----------------------------------------------------------------"
-# /usr/bin/parallel -j$1 python3 surfPow/surf_Pow.py $verbose $2 $SURFTYPE $WINDOW :::: /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/rc_list_$DATE.txt
-# echo "Surface reflectivity calculation completed"
-# echo "----------------------------------------------------------------"
+echo "Calculating surface reflectivity"
+echo "Surface type: $SURFTYPE"
+echo "Window: $WINDOW"
+echo "----------------------------------------------------------------"
+/usr/bin/parallel -j$1 python3 surfPow/surf_Pow.py $verbose $2 $SURFTYPE $WINDOW :::: /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/rc_list_$DATE.txt
+echo "Surface reflectivity calculation completed"
+echo "----------------------------------------------------------------"
 
-# echo "Removing data files"
-# rm -r /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/data/
-# rm -r /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/browse/
+echo "Removing data files"
+rm -r /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/data/
+rm -r /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/browse/
 
-# # make/append readme to show that range compression was completed on said date
-# echo "$2 range compression completed on $DATE" >> /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/README.txt
-# echo "----------------------------------------------------------------"
+# make/append readme to show that range compression was completed on said date
+echo "$2 range compression completed on $DATE" >> /zippy/MARS/targ/xtra/SHARAD/EDR/rangeCompress/$2/README.txt
+echo "----------------------------------------------------------------"
 
-# echo "Adding surface reflectivity measurements to postgres database"
+echo "Adding surface reflectivity measurements to postgres database"
 
-# python3 psql/sref/sref_psql_import.py $verbose $2
+python3 psql/sref/sref_psql_import.py $verbose $2
 
-# echo "----------------------------------------------------------------"
+echo "----------------------------------------------------------------"
 
-# # display run time
-# echo "Total Runtime: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
-# echo "----------------------------------------------------------------"
+# display run time
+echo "Total Runtime: $(($SECONDS / 3600))hrs $((($SECONDS / 60) % 60))min $(($SECONDS % 60))sec"
+echo "----------------------------------------------------------------"
